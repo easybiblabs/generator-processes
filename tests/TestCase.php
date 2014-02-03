4644a15a4953a03e6f2bc74275b6794f7a850ee6
Nils Adermann <naderman@naderman.de>
2014-02-03 15:25:26 +0100

    Add base test case in case we need helper methods

---
+++ tests/TestCase.php
@@ -0,0 +1,19 @@
+<?php
+/*
+ * This file is part of easybib/generator-processes
+ *
+ * (c) Imagine Easy Solutions, LLC
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author   Nils Adermann <naderman@naderman.de>
+ * @license  BSD-2-Clause
+ * @link     http://www.imagineeasy.com
+ */
+
+namespace EasyBib\Process\Test;
+
+class TestCase extends \PHPUnit_Framework_TestCase
+{
+}
