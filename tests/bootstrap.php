c547017eb94898e4128bd9526b7425a9e87b7b28
Nils Adermann <naderman@naderman.de>
2014-02-03 15:24:45 +0100

    Add phpunit configuration and bootstrap

---
+++ tests/bootstrap.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * This file is part of easybib/generator-processes
+ *
+ * (c) Imagine Easy Solutions, LLC
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author   Nils Adermann <naderman@naderman.de>
+ * @license  BSD-2-Clause
+ * @link     http://www.imagineeasy.com
+ */
+
+$loader = require __DIR__.'/../vendor/autoload.php';
+$loader->addPsr4('EasyBib\Process\Test\\', __DIR__);
